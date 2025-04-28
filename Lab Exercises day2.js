c97daaa0f32f1fe7f4837980f8fa7908dd0aee0e
abouthe1 <somuah408@gmail.com>
2025-04-28 13:00:19 +0100

    Submitting Lab Exercises day2.js

---
+++ Lab Exercises day2.js
@@ -0,0 +1,31 @@
+//while loop
+ let i = 0;
+ while (i < mixed.length){
+    console.log(mixed[i]);
+    i++;
+ }
+ 
+ let j = 0
+ do{
+     console.log(mixed[j]);
+     j++;
+     console.log("j is: " + j)
+ }
+ while(j < mixed.length)
+     console.log("j is completed: " + j);
+
+
+ // if statement
+
+ let classscore = 100
+let grade = ""
+if (score >= 90) {
+  grade = "A"}
+   else if (score >= 80) {
+  grade = "B"}
+   else if (score >= 70) {
+  grade = "C"
+} else {
+  grade = "F"
+}
+console.log(results);
